5d7f40e87899a2e372f71b6a13507887eddebfe1
tiagowilsonrft <tiagowilsonrft@gmail.com>
2025-02-10 14:43:31 -0300

    Continuação CRUD Delete e Update

--- apps/registro_hora_extra/urls.py
+++ apps/registro_hora_extra/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from .views import (HoraExtraList,
-                    #FuncionarioEdit,
-                    #FuncionarioDelete,
-                    #FuncionarioNovo
+                    HoraExtraEdit,
+                    HoraExtraDelete,
+                    HoraExtraNovo
     )
 
 urlpatterns = [
     path('', HoraExtraList.as_view(),  name='list_hora_extra'),
-   # path('editar/<int:pk>/', FuncionarioEdit.as_view(),  name='update_funcionario'),
-    # path('delete/<int:pk>/', FuncionarioDelete.as_view(),  name='delete_funcionario'),
-    # path('novo/', FuncionarioNovo.as_view(),  name='create_funcionario'),
+    path('novo/', HoraExtraNovo.as_view(),  name='create_hora_extra'),
+    path('editar/<int:pk>/', HoraExtraEdit.as_view(),  name='update_hora_extra'),
+    path('delete/<int:pk>/', HoraExtraDelete.as_view(),  name='delete_hora_extra'),
 ]
