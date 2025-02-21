f63711255fa0040aec7fb2dce0c3111c5ffc8f65
tiagowilsonrft <tiagowilsonrft@gmail.com>
2025-02-21 19:52:24 -0300

    mudando o statc root

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -109,7 +109,7 @@ STATICFILES_DIRS = [
 
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
